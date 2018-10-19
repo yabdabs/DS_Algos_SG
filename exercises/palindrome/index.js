f29b56e99ab1b0c84303ed29ab687310d0c92751
Abdul Khan <abdul.k.khan75@gmail.com>
2018-10-18 23:13:10 -0400

    finish palindrome on my own

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,33 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+
+//     let reversed  = str.split('').reverse().join('')
+
+//     if (reversed === str) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+
+
+// Loops through the array, checking first with last, continuing on respectively.
+// This solution is not the most efficient b/c we only need to loop
+// till the middle of the array. This is iterating through the entire array.
+// Make sure to tell this in the interview 
+const palindrome = (str) => {
+
+    strArr = str.split('')
+
+    return strArr.every( (char, index, arr) => {
+        return char === arr[arr.length - index -1]
+    })
+
+}
+
+// palindrome('abba')
 
 module.exports = palindrome;
