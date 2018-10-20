2b81fa7956b32cc00159a5dc5c35b36eb15ae26c
Abdul Khan <abdul.k.khan75@gmail.com>
2018-10-20 14:43:30 -0400

    finish reverse Int and maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,72 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+/*
+function maxChar(str) {
+
+    // create  an object with key as letter, and value as the count for that letter
+    let obj =  {}
+    let max = 0
+    let char
+
+    // loop through the string
+    for( let i = 0; i < str.length; i++) {
+
+        if (!obj[str.charAt(i)]) {
+            obj[str.charAt(i)] = 1
+        }
+        else {
+            obj[str.charAt(i)] ++
+        }
+
+    }
+
+    console.log(obj)
+
+    for (let key in obj) {
+        if(obj.hasOwnProperty(key)) {
+            console.log('OBJ,KEY', obj[key])
+            if (obj[key] > max) {
+                max = obj[key]
+                char = key
+            }
+        }
+    }
+    console.log(char)
+    return char
+
+}
+*/
+
+function maxChar(str) {
+
+    // create  an object with key as letter, and value as the count for that letter
+    let obj =  {}
+    let max = 0
+    let char
+
+    // loop through the string
+    for ( char of str) {
+        obj[char] = obj[char] ++ || 1
+    }
+
+    console.log(obj)
+
+    for (let key in obj) {
+        if(obj.hasOwnProperty(key)) {
+            console.log('OBJ,KEY', obj[key])
+            if (obj[key] > max) {
+                max = obj[key]
+                char = key
+            }
+        }
+    }
+    console.log(char)
+    return char
+
+}
+
+maxChar('a')
+
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+/*
+function reverseInt(n) {
+
+    let strNum = n.toString().split('').reverse().join('')
+
+    if (n < 0) {
+        return parseInt(strNum) * -1
+    }
+
+    return parseInt(strNum)
+
+}
+*/
+
+const reverseInt = (n) => {
+
+    let reverse = n.toString().split('').reverse().join('')
+
+    return parseInt(reverse) * Math.sign(n)
+
+}
 
 module.exports = reverseInt;
