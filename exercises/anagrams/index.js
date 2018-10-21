f410fbcf589243ee7fe743dc31a5edbf75f5d0e1
Abdul Khan <abdul.k.khan75@gmail.com>
2018-10-21 03:59:09 -0400

    finish my own version of anagrams before watching vide

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,60 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+
+// loop through string
+// store each char in obj as key, and count as the value. convert char to lowercase
+// do that for both strings
+// then compare the objects keys and values and see if they are equal to eachother. use for in
+
+function anagrams(stringA, stringB) {
+
+    let objA = {}
+    let objB = {}
+    let isEqual = true
+    stringA = stringA.toLowerCase()
+    stringB = stringB.toLowerCase()
+
+    for (char of stringA) {
+        if(/^[a-zA-Z]$/.test(char)){
+            if (!objA[char]) {
+                objA[char] = 1
+            }
+            else {
+                objA[char] ++
+            }
+        }
+    }
+
+    for (char of stringB) {
+        if(/^[a-zA-Z]$/.test(char)){
+            if (!objB[char]) {
+                objB[char] = 1
+            }
+            else {
+                objB[char] ++
+            }
+        }
+        // if(/^[a-zA-Z]$/.test(char)){
+        //     objB[char.toLowerCase()] = objB[char.toLowerCase()] ++ || 1
+        // }
+    }
+
+    if (Object.keys(objA).length != Object.keys(objB).length) {
+        isEqual = false
+    }
+
+    for (key in objA) {
+        if (objA.hasOwnProperty(key)) {
+            if(objA[key] !== objB[key]) {
+                isEqual = false
+            }
+        }
+    }
+
+    return isEqual
+}
+
+anagrams('Whoa! Hi!', 'Hi! Whoa!')
 
 module.exports = anagrams;
